48c4a1d0da73aa382a018ecba83c44e55123d2ba
mpadilhaa <mpadilhaaac@gmail.com>
2023-06-08 19:08:56 -0300

    criação das rotas

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
