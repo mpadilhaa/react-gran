ae799cfc69f9844652027e73714e77d4ecfb154b
mpadilhaa <mpadilhaaac@gmail.com>
2023-06-08 20:07:57 -0300

    estilizado navbar, footer, register, login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,34 @@
 import "./Auth.css";
 
+//components
+import { Link } from "react-router-dom";
+
+//Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="register">
+      <h2>ReactGran</h2>
+      <p className="subtitle">Cadastre-se para usar o ReactGran</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="" id="" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" name="" id="" placeholder="Senha" />
+        <input type="password" name="" id="" placeholder="Confirme sua Senha" />
+
+        <input type="submit" value="Cadastrar" />
+      </form>
+
+      <p>
+        Já tem conta? <Link to="/login"> Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
